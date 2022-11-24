fd339258e26546d275f7e4e05cc1902e310c3236
Maurice Mueller <maurice.mueller@bitraven.de>
2022-11-24 09:34:05 +0100

    basic structure for input

--- src/main/java/dhbw/Timetracker.java
+++ src/main/java/dhbw/Timetracker.java
@@ -1,8 +1,22 @@
 package dhbw;
 
+import java.util.Scanner;
+
 public class Timetracker {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Haben Sie bereits ein Konto? (j/n)");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if(input.equalsIgnoreCase("j") ) {
+            // TODO do login
+        } else if (input.equalsIgnoreCase("n")) {
+            // TODO register
+        } else {
+            System.out.println("Falsche Eingabe.");
+        }
+
+        System.out.println("Input: " + input);
     }
 }
