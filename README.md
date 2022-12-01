bd44ede9673fac18e37b2aea58029199cd399ecd
Maurice Mueller <maurice.mueller@bitraven.de>
2022-12-01 10:30:58 +0100

    added maven and junit 5

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ This is a test project for DHBW TINF20B5.
 
 ## TODOs
 
-- implement build tool
 - TDD
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    void testTest() {
+        System.out.println("Hello world.");
+    }
+}
